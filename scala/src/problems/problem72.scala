7c64cc9cdf9b037259cc574c5a58b7255468567a
Toma <cream@MacMiniCream.local>
2014-03-07 15:47:12 +0900

    Solved problem 73.

--- scala/src/problems/problem72.scala
+++ scala/src/problems/problem72.scala
@@ -5,19 +5,20 @@ import lib.Mathematics
 object problem72 extends App {
 
   val up = Mathematics.sqrt( 1000000000 )
-  val low = BigInt( 0 )
+  val low = BigInt( 10 )
 
- val same3LastDigits = ( low to up ).filter( i => (i*i).toString.length >=3 && (i*i).toString.substring( (i*i).toString.length-3, (i*i).toString.length).distinct.length == 3 )
- 
- // has to remove all the 13499999 since more than 3 identical
+  // get all powers which are ending with 3 same last digits
+ val same3LastDigits = ( low to up ).filter( i => {val pow= i*i; pow.toString.substring( pow.toString.length-3, pow.toString.length).distinct.length == 1  } )
 
  println( same3LastDigits )
- val size = same3LastDigits.length
  
-  println(s"how many such numbers  : $size")
-  // response 854
-  
-  val k = 984751243
-  println( "last three car " + k.toString.substring( k.toString.length-3,k.toString.length))
+ // remove all the VGAKKK+ pattern, since more than 3 K identical symbol at the bottom of the string
+ val sameExclusive3LastDigits = ( same3LastDigits ).filter( i => {val pow= i*i; !pow.toString.substring( pow.toString.length-4, pow.toString.length-3).equals( pow.toString.substring( pow.toString.length-3, pow.toString.length-2)) } )
+
+ println( sameExclusive3LastDigits )
+ val size = sameExclusive3LastDigits.length
+ 
+ println(s"how many such numbers  : $size")
+ // response 127
   
 }
